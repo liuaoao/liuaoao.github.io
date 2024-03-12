c25b0e6f49c3519262427ede465bd1087d6d10fa
liuaoao <825417811@qq.com>
2024-03-13 00:34:24 +0800

    changename

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
     return ( 
         <nav className="navbar">
-            <h1>The Hongzheng Blog</h1>
+            <h1>The liuaoao Blog</h1>
             <div className="links">
                 {/* link的话不用refresh */}
                 <Link to="/">Home</Link>
